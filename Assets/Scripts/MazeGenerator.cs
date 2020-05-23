10899cd228b35daae9d8a34ec64ecedd345e05fe
Sandeep Nambiar <sandeep.turing@gmail.com>
2020-05-23 12:49:14 +0530

    Single commit after cleanup into new public repository.

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Flags]
+public enum WallState
+{
+    // 0000 -> NO WALLS
+    // 1111 -> LEFT,RIGHT,UP,DOWN
+    LEFT = 1, // 0001
+    RIGHT = 2, // 0010
+    UP = 4, // 0100
+    DOWN = 8, // 1000
+
+    VISITED = 128, // 1000 0000
+}
+
+public struct Position
+{
+    public int X;
+    public int Y;
+}
+
+public struct Neighbour
+{
+    public Position Position;
+    public WallState SharedWall;
+}
+
+public static class MazeGenerator
+{
+
+    private static WallState GetOppositeWall(WallState wall)
+    {
+        switch (wall)
+        {
+            case WallState.RIGHT: return WallState.LEFT;
+            case WallState.LEFT: return WallState.RIGHT;
+            case WallState.UP: return WallState.DOWN;
+            case WallState.DOWN: return WallState.UP;
+            default: return WallState.LEFT;
+        }
+    }
+
+    private static WallState[,] ApplyRecursiveBacktracker(WallState[,] maze, int width, int height)
+    {
+        // here we make changes
+        var rng = new System.Random(/*seed*/);
+        var positionStack = new Stack<Position>();
+        var position = new Position { X = rng.Next(0, width), Y = rng.Next(0, height) };
+
+        maze[position.X, position.Y] |= WallState.VISITED;  // 1000 1111
+        positionStack.Push(position);
+
+        while (positionStack.Count > 0)
+        {
+            var current = positionStack.Pop();
+            var neighbours = GetUnvisitedNeighbours(current, maze, width, height);
+
+            if (neighbours.Count > 0)
+            {
+                positionStack.Push(current);
+
+                var randIndex = rng.Next(0, neighbours.Count);
+                var randomNeighbour = neighbours[randIndex];
+
+                var nPosition = randomNeighbour.Position;
+                maze[current.X, current.Y] &= ~randomNeighbour.SharedWall;
+                maze[nPosition.X, nPosition.Y] &= ~GetOppositeWall(randomNeighbour.SharedWall);
+                maze[nPosition.X, nPosition.Y] |= WallState.VISITED;
+
+                positionStack.Push(nPosition);
+            }
+        }
+
+        return maze;
+    }
+
+    private static List<Neighbour> GetUnvisitedNeighbours(Position p, WallState[,] maze, int width, int height)
+    {
+        var list = new List<Neighbour>();
+
+        if (p.X > 0) // left
+        {
+            if (!maze[p.X - 1, p.Y].HasFlag(WallState.VISITED))
+            {
+                list.Add(new Neighbour
+                {
+                    Position = new Position
+                    {
+                        X = p.X - 1,
+                        Y = p.Y
+                    },
+                    SharedWall = WallState.LEFT
+                });
+            }
+        }
+
+        if (p.Y > 0) // DOWN
+        {
+            if (!maze[p.X, p.Y - 1].HasFlag(WallState.VISITED))
+            {
+                list.Add(new Neighbour
+                {
+                    Position = new Position
+                    {
+                        X = p.X,
+                        Y = p.Y - 1
+                    },
+                    SharedWall = WallState.DOWN
+                });
+            }
+        }
+
+        if (p.Y < height - 1) // UP
+        {
+            if (!maze[p.X, p.Y + 1].HasFlag(WallState.VISITED))
+            {
+                list.Add(new Neighbour
+                {
+                    Position = new Position
+                    {
+                        X = p.X,
+                        Y = p.Y + 1
+                    },
+                    SharedWall = WallState.UP
+                });
+            }
+        }
+
+        if (p.X < width - 1) // RIGHT
+        {
+            if (!maze[p.X + 1, p.Y].HasFlag(WallState.VISITED))
+            {
+                list.Add(new Neighbour
+                {
+                    Position = new Position
+                    {
+                        X = p.X + 1,
+                        Y = p.Y
+                    },
+                    SharedWall = WallState.RIGHT
+                });
+            }
+        }
+
+        return list;
+    }
+
+    public static WallState[,] Generate(int width, int height)
+    {
+        WallState[,] maze = new WallState[width, height];
+        WallState initial = WallState.RIGHT | WallState.LEFT | WallState.UP | WallState.DOWN;
+        for (int i = 0; i < width; ++i)
+        {
+            for (int j = 0; j < height; ++j)
+            {
+                maze[i, j] = initial;  // 1111
+            }
+        }
+        
+        return ApplyRecursiveBacktracker(maze, width, height);
+    }
+}
---
+++ Assets/Scripts/MazeRenderer.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeRenderer : MonoBehaviour
+{
+
+    [SerializeField]
+    [Range(1, 50)]
+    private int width = 10;
+
+    [SerializeField]
+    [Range(1, 50)]
+    private int height = 10;
+
+    [SerializeField]
+    private float size = 1f;
+
+    [SerializeField]
+    private Transform wallPrefab = null;
+
+    [SerializeField]
+    private Transform floorPrefab = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var maze = MazeGenerator.Generate(width, height);
+        Draw(maze);
+    }
+
+    private void Draw(WallState[,] maze)
+    {
+
+        var floor = Instantiate(floorPrefab, transform);
+        floor.localScale = new Vector3(width, 1, height);
+
+        for (int i = 0; i < width; ++i)
+        {
+            for (int j = 0; j < height; ++j)
+            {
+                var cell = maze[i, j];
+                var position = new Vector3(-width / 2 + i, 0, -height / 2 + j);
+
+                if (cell.HasFlag(WallState.UP))
+                {
+                    var topWall = Instantiate(wallPrefab, transform) as Transform;
+                    topWall.position = position + new Vector3(0, 0, size / 2);
+                    topWall.localScale = new Vector3(size, topWall.localScale.y, topWall.localScale.z);
+                }
+
+                if (cell.HasFlag(WallState.LEFT))
+                {
+                    var leftWall = Instantiate(wallPrefab, transform) as Transform;
+                    leftWall.position = position + new Vector3(-size / 2, 0, 0);
+                    leftWall.localScale = new Vector3(size, leftWall.localScale.y, leftWall.localScale.z);
+                    leftWall.eulerAngles = new Vector3(0, 90, 0);
+                }
+
+                if (i == width - 1)
+                {
+                    if (cell.HasFlag(WallState.RIGHT))
+                    {
+                        var rightWall = Instantiate(wallPrefab, transform) as Transform;
+                        rightWall.position = position + new Vector3(+size / 2, 0, 0);
+                        rightWall.localScale = new Vector3(size, rightWall.localScale.y, rightWall.localScale.z);
+                        rightWall.eulerAngles = new Vector3(0, 90, 0);
+                    }
+                }
+
+                if (j == 0)
+                {
+                    if (cell.HasFlag(WallState.DOWN))
+                    {
+                        var bottomWall = Instantiate(wallPrefab, transform) as Transform;
+                        bottomWall.position = position + new Vector3(0, 0, -size / 2);
+                        bottomWall.localScale = new Vector3(size, bottomWall.localScale.y, bottomWall.localScale.z);
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
